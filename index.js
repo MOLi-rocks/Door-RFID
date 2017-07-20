f6c12e7d1c7d8f5ed9daf29c2fe1326378826fc3
ast850328 <ast850328@gmail.com>
2017-07-20 19:41:18 +0800

    RFID 開關門會推撥觸發者資料到頻道

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var SerialPort = require('serialport');
 var rp = require('request');
 const ENV = require('./env.js');
+const keyholders = require('./keyholders.js');
 
 // set RFID reader
 var reader = new SerialPort(ENV.SERIAL_PORT, {
@@ -15,19 +16,41 @@ reader.on('data', function(buf) {
   var code = buf.readUIntLE(2);
 
   // verify card number when 'enter'
+  let stop = false;
   if (code === 40) {
-    if (cache == ENV.ID) {
-      rp.post({url: `${ENV.LOCK_SERVER}/switch`}, function(err, res, body){
-        console.log(body);
-      });
+    for (let keyholder of keyholders) {
+        for (let card of keyholder.cards) {
+            if (card.id === cache) {
+                rp.post({
+                    url: `${ENV.LOCK_SERVER}/switch`,
+                    form: {
+                        token: ENV.TOKEN,
+			message: formatMessage(keyholder, card.title)
+                    }
+                }, function (err, res, body) {
+                    console.log(body);
+                });
+                stop = true;
+                break;
+            }
+        }
+        if (stop) {
+            break;
+        }
     }
 
     // clear card number buffer
     cache = '';
-  }
+  }	 
 
   // record card number (ignore 0 & enter)
   if (code !== 0 && code !== 40) {
-    cache += code-29;
+    cache += (code-29) % 10;
   }
 })
+
+function formatMessage(keyholder, card) {
+  console.log(keyholder.telegram);
+  let message = `${keyholder.name} (@${keyholder.telegram}) 用 ${card}`;
+  return message;
+}
