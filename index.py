2e469432a48fb4bc4e01262ee69f18e7d35aaa70
safefly <ast850328@gmail.com>
2018-03-30 13:40:48 +0800

    add init function and "device not found" exception

--- index.py
+++ index.py
@@ -6,26 +6,9 @@ import json
 import requests
 from pprint import pprint
 
-# use cardID find keyholders
-def findKeyholders(cardID):
-    for keyholder in keyholders:
-        try:
-            card = keyholder['cards'][cardID]
-            if card is not None:
-                pprint(keyholder)
-                message = keyholder['name'] + ' (@' + keyholder['telegram'] + ') 用 '+ card
-                return message
-        except KeyError:
-            continue
-    return False
-
 # read env and keyholders
 ENV = json.load(open('env.json'))
 keyholders = json.load(open('keyholders.json'))
-
-# device id
-VENDOR_ID = int(ENV['VENDOR_ID'], 16)
-PRODUCT_ID = int(ENV['PRODUCT_ID'], 16)
 
 # reader key page
 key_pages = [
@@ -40,29 +23,56 @@ key_pages = [
     'KP9', 'KP0', '\\', 'App', 'Pow', 'KP=', 'F13', 'F14' 
 ]
 
-# find USB devices
-reader = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+# set reader and endpoint
+reader = None
+endpoint = None
 
-# wether found it
-if reader is None:
-    raise ValueError('Device not found')
+# use cardID find keyholders
+def findKeyholders(cardID):
+    for keyholder in keyholders:
+        try:
+            card = keyholder['cards'][cardID]
+            if card is not None:
+                pprint(keyholder)
+                message = keyholder['name'] + ' (@' + keyholder['telegram'] + ') 用 '+ card
+                return message
+        except KeyError:
+            continue
+    return False
 
-# to unload the kernel driver, so it does not output anymore the Ids on the console
-if reader.is_kernel_driver_active(0):
+# init device
+def init():
+    global reader
+    global endpoint
+    # device id
+    VENDOR_ID = int(ENV['VENDOR_ID'], 16)
+    PRODUCT_ID = int(ENV['PRODUCT_ID'], 16)
+
+    # find USB devices
+    reader = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+
+    # wether found it
+    if reader is None:
+        raise ValueError('Device not found')
+
+    # to unload the kernel driver, so it does not output anymore the Ids on the console
+    if reader.is_kernel_driver_active(0):
+        try:
+            reader.detach_kernel_driver(0)
+        except usb.core.USBError as e:
+            sys.exit("Could not detatch kernel driver: %s" % str(e))
+
+    # set configuration
     try:
-        reader.detach_kernel_driver(0)
+        reader.set_configuration()
+        reader.reset()
     except usb.core.USBError as e:
-        sys.exit("Could not detatch kernel driver: %s" % str(e))
+        sys.exit("Could not set configuration: %s" % str(e))
 
-# set configuration
-try:
-    reader.set_configuration()
-    reader.reset()
-except usb.core.USBError as e:
-    sys.exit("Could not set configuration: %s" % str(e))
+    #print('test')
+    endpoint = reader[0][(0,0)][0]
 
-#print('test')
-endpoint = reader[0][(0,0)][0]
+init()
 
 while True:
     try:
@@ -73,9 +83,9 @@ while True:
             if data is not 0 or 40:
                 cardID += key_pages[data]
         cardID = cardID.rstrip()
-        message = findKeyholders(cardID) 
+        message = findKeyholders(cardID)
+        print(message)
         if message is not False:
-            print(message)
             response = requests.post(
                 ENV['LOCK_SERVER']+'/switch',
                 data = {
@@ -83,7 +93,12 @@ while True:
                     "message": message
                 }
             )
+            print(response.text)
     except usb.core.USBError as e:
-        #print(str(e))
+        cardID = ''
+        data = ''
+        print(str(e))
+        if e.errno is 19:
+            init()
         continue
 
